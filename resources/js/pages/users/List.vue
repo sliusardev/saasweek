e827edb25c699feb0bdae9f0756f97d6d52b5fc5
slusarvitaliy <slusarvitaliy@gmail.com>
2025-06-13 22:57:48 +0300

    add user management features with dashboard and user list routes, sidebar updates, and data table trait

--- resources/js/pages/users/List.vue
+++ resources/js/pages/users/List.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { defineProps } from 'vue';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { Trash, UserPen } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -9,14 +19,75 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: route('users.list'),
     },
 ];
+
+defineProps<{
+    users: {
+        data: Array<{
+            id: number,
+            name: string,
+            email: string,
+            phone: string,
+            created_at: string,
+            roles: string[]
+        }>
+    }
+}>();
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Users" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-
+        <div class="p-5">
+            <Table>
+            <TableCaption>A list of users.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>ID</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Phone</TableHead>
+                    <TableHead>Roles</TableHead>
+                    <TableHead>Created At</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow v-for="user in users.data" :key="user.id">
+                    <TableCell>{{ user.id }}</TableCell>
+                    <TableCell>{{ user.name }}</TableCell>
+                    <TableCell>{{ user.email }}</TableCell>
+                    <TableCell>{{ user.phone }}</TableCell>
+                    <TableCell>{{ user.roles }}</TableCell>
+                    <TableCell>{{ user.created_at }}</TableCell>
+                    <TableCell>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger :as-child="true">
+                                <Button
+                                    size="icon"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" class="w-full">
+                                <DropdownMenuItem :as-child="true">
+                                    <Link class="block w-full" :href="route('users.edit', user)" prefetch as="button">
+                                        <UserPen class="mr-2 h-4 w-4" />
+                                        Edit
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem :as-child="true">
+                                    <Link class="block w-full" :href="route('users.delete', user)" prefetch as="button">
+                                        <Trash class="mr-2 h-4 w-4" />
+                                        Delete
+                                    </Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
         </div>
     </AppLayout>
 </template>
